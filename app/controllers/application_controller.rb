5c3ee390eae7b1507623df4b77dc61cd8c0b77c6
David De Anda <omarandstuff@gmail.com>
2022-06-02 00:10:49 -0500

    [ADD] simple request auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
-  def index
+  attr_reader :current_user
+
+  def entry
     render html: '', layout: true
+  end
+
+  protected
+
+  def authenticate_request
+    @current_user = User.find_by_id(request_authorization_token)
+    head :unauthorized unless @current_user
+  end
+
+  private
+
+  def request_authorization_token
+    request.headers['Authorization']&.split&.last
   end
 end
