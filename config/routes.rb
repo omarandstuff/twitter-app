39c322ff58a9fd438d0d87452a67332f3ce65f51
David De Anda <omarandstuff@gmail.com>
2022-06-01 23:52:40 -0500

    [FIX] app name

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  root 'application#index'
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      post 'log_in', to: 'authentication#log_in'
+      post 'sign_up', to: 'authentication#sign_up'
+
+      get 'tweets', to: 'tweets#index'
+      post 'tweets', to: 'tweets#create'
+    end
+  end
+
+  root 'application#entry'
+  match '*path', to: 'application#entry', via: :get
 end
